0e3e0156c03cb27592707838fd1730d5a5657bfb
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-09-30 00:33:49 -1100

    Update app.py

--- app.py
+++ app.py
@@ -1126,6 +1126,6 @@ def serve_audio(filename):
     return send_from_directory(OUTPUT_DIR, filename)
 
 if __name__ == '__main__':
-     app.run(debug=True,host="0.0.0.0", port=8000)
+     app.run(debug=True)
 
 
