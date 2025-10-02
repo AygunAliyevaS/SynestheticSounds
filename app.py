d615d6ed3b08d4fd5a12f53d4c96714db3d8f541
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-10-02 09:54:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def serve_index():
-    return render_template("index.html")
+    return 'everything is good'
 
 # Add route for serving GIFs (referenced in the HTML)
 @app.route("/gif/<filename>")
