f04011a1ed95644fc1dc8ddc579b84609cd53222
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-10-02 10:18:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def serve_index():
-    return 'everything is good'
+    return render_template("index.html")
 
-# Add route for serving GIFs (referenced in the HTML)
 @app.route("/gif/<filename>")
 def serve_gif(filename):
     return send_from_directory("static", filename)
+
+# Health check route (for Azure)
+@app.route("/robots933456.txt")
+def robots():
+    return "User-agent: *\nDisallow:", 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
