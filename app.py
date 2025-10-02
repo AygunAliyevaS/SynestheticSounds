0d2f7d5df486db3ba0a62aace40264ab7d48b980
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-10-02 11:14:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def serve_gif(filename):
 @app.route("/robots.txt")
 def robots():
     return "User-agent: *\nDisallow:", 200, {"Content-Type": "text/plain"}
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
