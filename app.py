84ea51c2456424c37cdefe4918e8927d912f8c34
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-10-04 20:57:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def serve_index():
-    return 'giood'
-
-@app.route("/gif/<filename>")
-def serve_gif(filename):
-    return send_from_directory("static", filename)
-
-# Health check routes (for Azure probes)
-@app.route("/robots933456.txt")
-@app.route("/robots.txt")
-def robots():
-    return "User-agent: *\nDisallow:", 200, {"Content-Type": "text/plain"}
-@app.route("/health")
-def health():
-    return "OK", 200
+def home():
+    return "giood"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
